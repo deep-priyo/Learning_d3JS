9974a6de3404acb7cb98260da90934913daa2055
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2025-03-16 17:59:55 +0530

    react files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import SmileyFace from "./Components/SmileyFace.jsx";
+import D3Selecet from "./Components/d3selecet.jsx";
+import DataDatum from "./Components/DataDatum.jsx";
+import BarChartNormal from "./Components/BarChartNormal.jsx";
+import DyncamicRectBarChart from "./Components/DyncamicRectBarChart.jsx";
+import StyledBarGraph from "./Components/StyledBarGraph.jsx";
+
+const App = () => {
+    return (
+        <div>
+            <SmileyFace />
+            <D3Selecet/>
+            <DataDatum/>
+            <BarChartNormal/>
+            <DyncamicRectBarChart/>
+            <StyledBarGraph/>
+        </div>
+    )
+}
+export default App
